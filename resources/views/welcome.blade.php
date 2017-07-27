a0bc63b72a41753b80203af3a6d22b49e5530368
superdev810 <superdev810@gmail.com>
2017-07-28 04:20:16 +1000

    change the template views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,31 +2,8 @@
 
 @section('content')
     <div class="main-container">
-         <h1>Hackathon Starter</h1>
-         <p class="lead">A boilerplate for Laravel web applications.</p>
+         <h1>Amazon Buybox</h1>
+         <p class="lead">This is amazon buybox for amazon sellers</p>
 
-         <hr>
-
-         <div class="row">
-            <div class="col-sm-6">
-                <h2>Heading</h2>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
-                <p><a href="#" role="button" class="btn btn-default">View details »</a></p>
-            </div>
-            <div class="col-sm-6">
-                <h2>Heading</h2>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
-                <p><a href="#" role="button" class="btn btn-default">View details »</a></p>
-            </div>
-            <div class="col-sm-6">
-                <h2>Heading</h2>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
-                <p><a href="#" role="button" class="btn btn-default">View details »</a></p>
-            </div>
-            <div class="col-sm-6">
-                <h2>Heading</h2><p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
-                <p><a href="#" role="button" class="btn btn-default">View details »</a></p>
-            </div>
-        </div>
     </div>
 @stop
